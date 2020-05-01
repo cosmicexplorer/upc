2f075478b83ce0c41659bfe1c03ea4d79af30c13
Danny McClanahan <1305167+cosmicexplorer@users.noreply.github.com>
2020-05-01 12:33:13 -0700

    add process_executor beginnings

---
+++ local/virtual-cli/client/standalone/process-executor/ProcessExecutor.scala
@@ -0,0 +1,22 @@
+package upc.local.virtual_cli.client.standalone.process_executor
+
+import upc.local._
+import upc.local.thrift_socket._
+import upc.local.virtual_cli.client._
+
+
+object ProcessExecutor extends MainWrapper {
+  override def virtualizedMainMethod(args: Array[String]): Int = {
+    val (exeClient, reapClient, initialDigest, cmd) = args match {
+      case Array(exeServerPath, reapServerPath, initialFingerprint, initialSizeBytes, "--", cmd*) => (
+        ThriftUnixClient(exeServerPath).get,
+        ThriftUnixClient(reapServerPath).get,
+        DirectoryDigest(Digest.fromFingerprintHex(initialFingerprint, initialSizeBytes.toInt)),
+        cmd,
+      )
+      case x => throw new RuntimeException(s"unrecognized arguments $x. expected:  [VAR=VAL...] command [args...]")
+    }
+
+    cmd.map
+  }
+}
